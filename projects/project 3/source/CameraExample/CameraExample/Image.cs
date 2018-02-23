6016c8bd312c436ed69c5633143e7919c1be38cd
leviwp <leviwp48@gmail.com>
2018-02-23 12:24:30 -0800

    Continuing to work on getting API to function.

--- projects/project 3/source/CameraExample/CameraExample/Image.cs
+++ projects/project 3/source/CameraExample/CameraExample/Image.cs
@@ -22,14 +22,13 @@ namespace CameraExample
         public List<string> tags = new List<string>();
         bool imageCheck = false;
 
-        public void SubmitPic()
+        public void SetTags(List<string> tag_list)
+        {
+            tags = tag_list;
+        }
+        public void SubmitPic(int word_track)
         {
             imageCheck = false;
-
-            if (CheckBitmap() == true)
-            {
-                GetAPI();
-            }
 
             for (int i = 0; i < GetTagsLength(); i++)
             {
@@ -42,6 +41,10 @@ namespace CameraExample
             UpdatePoints();
         }
 
+        public int GetPoints()
+        {
+            return progress;
+        }
         public void SetBitmap(Bitmap map)
         {
              bitmap = Android.Graphics.Bitmap.CreateScaledBitmap(map, 1024, 768, true);
@@ -73,14 +76,6 @@ namespace CameraExample
                 return false;
             }
         }
-
-        public void UpdateLvl()
-        {
-            if(progress <= 100)
-            {
-                lvl++;
-            }
-        }
         
         public void UpdatePoints()
         {
@@ -99,6 +94,12 @@ namespace CameraExample
                     progress = progress - 20;
                 }
             }
+
+            if (progress <= 100)
+            {
+                lvl++;
+                progress = 0;
+            }
         }
 
         public string GetWords(int spot)
@@ -111,64 +112,7 @@ namespace CameraExample
             return words[spot];
         }
 
-        public void GetAPI()
-        {
-
-            //convert bitmap into stream to be sent to Google API
-            string bitmapString = "";
-            using (var stream = new System.IO.MemoryStream())
-            {
-                bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 0, stream);
-
-                var bytes = stream.ToArray();
-                bitmapString = System.Convert.ToBase64String(bytes);
-            }
-
-            //credential is stored in "assets" folder
-            string credPath = "google_api.json";
-            Google.Apis.Auth.OAuth2.GoogleCredential cred;
-
-            //Load credentials into object form
-            using (var stream = Assets.Open(credPath))
-            {
-                cred = Google.Apis.Auth.OAuth2.GoogleCredential.FromStream(stream);
-            }
-            cred = cred.CreateScoped(Google.Apis.Vision.v1.VisionService.Scope.CloudPlatform);
-
-            // By default, the library client will authenticate 
-            // using the service account file (created in the Google Developers 
-            // Console) specified by the GOOGLE_APPLICATION_CREDENTIALS 
-            // environment variable. We are specifying our own credentials via json file.
-            var client = new Google.Apis.Vision.v1.VisionService(new Google.Apis.Services.BaseClientService.Initializer()
-            {
-                ApplicationName = "mobile-apps-tutorial",
-                HttpClientInitializer = cred
-            });
-
-            //set up request
-            var request = new Google.Apis.Vision.v1.Data.AnnotateImageRequest();
-            request.Image = new Google.Apis.Vision.v1.Data.Image();
-            request.Image.Content = bitmapString;
-
-            //tell google that we want to perform label detection
-            request.Features = new List<Google.Apis.Vision.v1.Data.Feature>();
-            request.Features.Add(new Google.Apis.Vision.v1.Data.Feature() { Type = "LABEL_DETECTION" });
-            var batch = new Google.Apis.Vision.v1.Data.BatchAnnotateImagesRequest();
-            batch.Requests = new List<Google.Apis.Vision.v1.Data.AnnotateImageRequest>();
-            batch.Requests.Add(request);
-
-            //send request.  Note that I'm calling execute() here, but you might want to use
-            //ExecuteAsync instead
-            var apiResult = client.Images.Annotate(batch).Execute();
-            
-            foreach (var item in apiResult.Responses[0].LabelAnnotations)
-            {
-                tags.Add(item.Description);
-            }
-            //string question = string.Format("is this (a(n)) {0}?", tags[0]);
-            //TextView output = FindViewById<TextView>(Resource.Id.gameText);
-            //output.Text = question;
-        }
+       
 
 
     }
--- projects/project 3/source/CameraExample/CameraExample/visionGame.cs
+++ projects/project 3/source/CameraExample/CameraExample/visionGame.cs
@@ -20,7 +20,7 @@ namespace CameraExample
     {
         // Creates Image object
         Image image = new Image();
-
+        int word_track = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -39,7 +39,11 @@ namespace CameraExample
             }
 
             Button submit = FindViewById<Button>(Resource.Id.btn_submit);
-            submit.Click += image.SubmitPic();
+            //submit.Click += image.SubmitPic(word_track);
+
+            ProgressBar bar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            bar.Max = 100;
+            bar.Progress = image.GetPoints();
         }
 
       
@@ -93,16 +97,77 @@ namespace CameraExample
 
             // Sets image on GameView layout
             ImageView takenPic = FindViewById<ImageView>(Resource.Id.gameImage);
-            if (image.CheckBitmap() != null)
+
+            if (image.CheckBitmap() != false)
             {
                 takenPic.SetImageBitmap(image.GetBitmap());
             }
+            
+            //convert bitmap into stream to be sent to Google API
+            string bitmapString = "";
+            using (var stream = new System.IO.MemoryStream())
+            {
+                image.GetBitmap().Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 0, stream);
+
+                var bytes = stream.ToArray();
+                bitmapString = System.Convert.ToBase64String(bytes);
+            }
+
+            //credential is stored in "assets" folder
+            string credPath = "google_api.json";
+            Google.Apis.Auth.OAuth2.GoogleCredential cred;
+
+            //Load credentials into object form
+            using (var stream = Assets.Open(credPath))
+            {
+                cred = Google.Apis.Auth.OAuth2.GoogleCredential.FromStream(stream);
+            }
+            cred = cred.CreateScoped(Google.Apis.Vision.v1.VisionService.Scope.CloudPlatform);
+
+            // By default, the library client will authenticate 
+            // using the service account file (created in the Google Developers 
+            // Console) specified by the GOOGLE_APPLICATION_CREDENTIALS 
+            // environment variable. We are specifying our own credentials via json file.
+            var client = new Google.Apis.Vision.v1.VisionService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                ApplicationName = "mobile-apps-tutorial",
+                HttpClientInitializer = cred
+            });
+
+            //set up request
+            var request = new Google.Apis.Vision.v1.Data.AnnotateImageRequest();
+            request.Image = new Google.Apis.Vision.v1.Data.Image();
+            request.Image.Content = bitmapString;
+
+            //tell google that we want to perform label detection
+            request.Features = new List<Google.Apis.Vision.v1.Data.Feature>();
+            request.Features.Add(new Google.Apis.Vision.v1.Data.Feature() { Type = "LABEL_DETECTION" });
+            var batch = new Google.Apis.Vision.v1.Data.BatchAnnotateImagesRequest();
+            batch.Requests = new List<Google.Apis.Vision.v1.Data.AnnotateImageRequest>();
+            batch.Requests.Add(request);
+
+            //send request.  Note that I'm calling execute() here, but you might want to use
+            //ExecuteAsync instead
+            var apiResult = client.Images.Annotate(batch).Execute();
+            List<string> details = new List<string>();
+
+            foreach (var item in apiResult.Responses[0].LabelAnnotations)
+            {
+                details.Add(item.Description);
+            }
+
+            image.SetTags(details);
+            //string question = string.Format("is this (a(n)) {0}?", tags[0]);
+            //TextView output = FindViewById<TextView>(Resource.Id.gameText);
+            //output.Text = question;
 
             // Dispose of the Java side bitmap.
             System.GC.Collect();
 
             Finish();
         }
+            
+        
 
        
 
